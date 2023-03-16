94a20bb466433d7f55561e55a76b7a445ffa2ca7
George Nicolaou <orionas.elite@gmail.com>
2023-03-16 16:55:17 +0200

    final

--- customer-email-exporter-based-on-product-purchase.php
+++ customer-email-exporter-based-on-product-purchase.php
@@ -61,11 +61,78 @@ define( 'CUSTOMEREM_PLUGIN_DIR',	plugin_dir_path( CUSTOMEREM_PLUGIN_FILE ) );
 // Plugin Folder URL
 define( 'CUSTOMEREM_PLUGIN_URL',	plugin_dir_url( CUSTOMEREM_PLUGIN_FILE ) );
 
+
+
 /**
  * Load the main class for the core functionality
  */
 require_once CUSTOMEREM_PLUGIN_DIR . 'core/class-customer-email-exporter-based-on-product-purchase.php';
 
+
+function get_orders_by_product_dropdown() {
+    // Get all products
+    $products = wc_get_products(array(
+        'limit' => -1,
+        'orderby' => 'title',
+        'order' => 'asc',
+    ));
+
+    // Build the product dropdown options
+    $options = '';
+    foreach ($products as $product) {
+        $options .= '<option value="' . $product->get_id() . '">' . $product->get_name() . '</option>';
+    }
+
+    // Output the product dropdown
+    $output = '<form method="POST">';
+    $output .= '<label for="product_dropdown">Select a product:</label>';
+    $output .= '<select name="product_dropdown" id="product_dropdown">' . $options . '</select>';
+    $output .= '<button type="submit">Get Orders</button>';
+    $output .= '</form>';
+
+    // Check if the form has been submitted
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['product_dropdown'])) {
+        // Get the selected product ID
+        $product_id = $_POST['product_dropdown'];
+
+        // Get all orders that contain the selected product
+        $args = array(
+            'meta_query' => array(
+                array(
+                    'key' => '_product_id',
+                    'value' => $product_id,
+                    'compare' => '=',
+                ),
+            ),
+        );
+        $orders = wc_get_orders($args);
+
+        // Loop through each order and get the customer's email address
+        $emails = array();
+        foreach ($orders as $order) {
+            $emails[] = $order->get_billing_email();
+        }
+
+        // Output the results
+        if (!empty($emails)) {
+            $output .= '<p>Customers who purchased ' . get_the_title($product_id) . ':</p>';
+            $output .= '<ul>';
+            foreach ($emails as $email) {
+                $output .= '<li>' . $email . '</li>';
+            }
+            $output .= '</ul>';
+        } else {
+            $output .= '<p>No customers have purchased ' . get_the_title($product_id) . '.</p>';
+        }
+    }
+
+    echo $output;
+}
+
+add_action('admin_menu', 'export_specific_product_emails_menu');
+function export_specific_product_emails_menu() {
+    add_submenu_page('woocommerce', 'Export Product Emails', 'Export Product Emails', 'manage_options', 'get_orders_by_product_dropdown', 'get_orders_by_product_dropdown');
+}
 /**
  * The main function to load the only instance
  * of our master class.
